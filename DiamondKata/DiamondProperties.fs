138fa8aa98648d98c996a3546301a8b3bfb6cf7d
Michael Falanga <mfalanga@manifestcorp.com>
2016-03-22 07:31:22 -0400

    Restrict values sent to property to uppercase letters

--- DiamondKata/DiamondProperties.fs
+++ DiamondKata/DiamondProperties.fs
@@ -1,11 +1,18 @@
 ﻿module DiamondProperties
 
 open System
+open FsCheck
 open FsCheck.Xunit
 
 let stringExists = not << String.IsNullOrWhiteSpace
 
-[<Property>]
+type Letters =
+    static member Char() =
+        Arb.Default.Char()
+        |> Arb.filter (fun c -> 'A' <= c && c <= 'Z')
+
+
+[<Property(Arbitrary = [| typeof<Letters> |] )>]
 let ``Diamond is non-empty`` (letter:char) =
     let actual = Diamond.make letter
     stringExists actual
